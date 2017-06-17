eb247b9988d320dc4646a64998b2c888c5ca1ec2
Kathleen French <kfrench09@gmail.com>
2017-06-17 18:28:59 -0400

    user list updating on sign in

--- index.js
+++ index.js
@@ -17,15 +17,40 @@ http.listen(3000, function(){
     console.log('server connection established. listening on *:3000');
 });
 
-// username obj 
+// user list obj 
 var usernames = {};
-var username 
 
-// clients
-var clients = {};
-var socketsOfClients = {};
+// user channels obj
+var userSockets = {};
 
 io.on('connection', function(socket){    
+
+// add user + set username
+    socket.on('set username', function(username){
+        if(usernames[username] === undefined){
+            usernames[username] = socket.id;
+            userSockets[socket.id] = username;
+            usernameAvailable(socket.id, username);
+            userJoined(username);
+            socket.emit('newUser', username);
+            //socket.broadcast.emit('updateLogs', username + ' has joined the chat');
+            socket.emit('updateUsers', usernames);
+            console.log('update users if firing from the index');
+       /* } else {
+            usernameInUse(socket.id, username);  */
+        }
+    })
+
+    // disconnect
+    socket.on('disconnect', function(){
+        var username = usernames[[socket.username]]
+        delete username;
+        io.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updateLogs', username + ' has left the chat');
+    });
+
+
+/*
     // add a user
     socket.on('newUser', function(username){
         socket.username = username;
@@ -37,9 +62,29 @@ io.on('connection', function(socket){
 
     // send a message
     socket.on('sendMessage', function(msg){
+        var sender;
+
+        if (msg.socketName){
+            sender = userSockets[socket.id]
+        } else {
+            sender = msg.source;
+        }
+
+        if (msg.target == "everyone") {
+            io.sockets.emit('message', 
+            {   "source": sender,
+                "message": msg.message,
+                "target": msg.target })
+        } else {
+            io.sockets.sockets[usernames[msg.target]].emit('message',
+            {   "source": sender,
+                "message": msg.message,
+                "target": msg.target })
+        }
         console.log('message: ' + msg);
-        io.emit('sendMessage', msg);
+        io.emit('sendMessage', socket.username, msg);
     })
+  
 
     // disconnect
     socket.on('disconnect', function(){
@@ -47,9 +92,36 @@ io.on('connection', function(socket){
         io.sockets.emit('updateusers', username);
         socket.broadcast.emit('updateLog', 'Server', socket.username + ' has left the chat');
     });
-
+  */
 
 
 })
+
+function userJoined(username){
+    Object.keys(userSockets).forEach(function(socketId){
+        io.sockets.sockets[socketId].emit('userJoined', { "username" : username });
+    })
+}
+
+function userLeft(username){
+    io.sockets.emit('userleft', { "username" : username });
+}
+
+function usernameAvailable(socketId, username){
+    setTimeout(function(){
+        console.log(username + ' has joined the chat. their individual chat id is: ' + socketId);
+
+        io.sockets.sockets[socketId].emit('welcome', { "username" : username, "currentusers" : JSON.stringify(Object.keys(usernames)) });
+    }, 500)
+}
+
+
+/*
+function usernameInUse(socketId, username){
+    setTimeout(function(){
+        io.sockets.sockets[socketId].emit('error', { "usernameTaken" : true })
+    })
+}
+*/
 
 
--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 var socket = io.connect('http://localhost:3000/');
-socket.on('connect', newUser);
-socket.on('updateUsers', updateUsers);
+var thisUser;
 
+
+/*
 $(function () {
     $('form').submit(function(){
         socket.emit('sendMessage', $('#m').val());
@@ -9,9 +10,14 @@ $(function () {
         return false;
     });
     socket.on('sendMessage', function(msg){
-        $('#messages').append($('<li>').text(msg));
+        $('<span class="message">' + '[' + getTime() + '] ' + 'strong>' + username + ':</strong> ' + msg + '</span><br/>').insertAfter($('#log'))
+       // $('#messages').append($('<li>').text(msg));
     });
 });
+
+*/
+
+// need update logs
 
 function newUser() {
     var usernamePopUp = swal({
@@ -23,13 +29,21 @@ function newUser() {
         inputPlaceholder: "...right here"
     }, function(inputValue){
         if (inputValue === false) return false;
+
         if (inputValue === "") {
              swal.showInputError("enter a valid username, dummy");
              return false
          }
+
          inputValue = inputValue.replace(/<(?:.|\n)*?>/gm, '');
-         swal("Nice!", "cool, start chatting " + inputValue);
-         socket.emit('adduser', inputValue);
+         swal("## good2go ##!", "cool, start chatting " + inputValue);
+         thisUser = inputValue;
+
+         socket.emit('set username', thisUser);
+
+         console.log('this user: ' + thisUser)
+         console.log('username set as: ' + inputValue)
+         //$('#user-list').append('<li>').text(inputValue);
      });
 }
 
@@ -47,9 +61,87 @@ function getTime() {
     return hours + ':' + minutes + ':' + seconds;
 }
 
-function updateUsers(data) {
-    $('#users').empty();
-    $.each(data, function(key, value) {
-        $('#users').append('<div class="username"><i class="fa fa-user" aria-hidden="true"></i> ' + key + '</div>');
+/*
+function updateUsers(thisUser) {
+    $('#user-list').empty();
+    $.each(thisUser, function(key, value) {
+        $('#user-list').append('<li class="username"><i class="fa fa-user" aria-hidden="true"></i> ' + key + '</div>');
     });
 }
+*/
+
+
+function updateUsers(usernames){
+    $('#user-list').empty();
+    $.each(usernames, function(key, value){
+        $('#user-list').append('<div class="username"><i class="fa fa-user" aria-hidden="true"></i> ' + key + '</div>');
+    });
+    
+    /*
+    $('#user-list').append('<li><i class="fa fa-user" aria-hidden="true"></i>' + usernames.thisUser + '</li>');
+    console.log('this user update users: ' + thisUser);
+    
+
+    $('#user-list').append($('<option></option>').val(username).html(username));
+    if (notify && (thisUser !== username) && (thisUser !== 'everyone')){
+        $('#user-list').append('<span class="admin">==>' + username + " just joined <==<br/>")
+    }
+    */
+}
+
+
+/*
+function updateLog(username){
+    Object.keys(socketsOf)
+    $('#log').empty();
+    $.each(username, function(key, value){
+        $('log').append('<li class="notice">')
+    })
+}
+
+function loadMessage(username, msg){
+    $('<span class="message">' + '[' + getTime() + '] ' + 'strong>' + username + ':</strong> ' + msg + '</span><br/>').insertAfter($('#log'))
+}
+
+
+
+function updateUsers(username, notify){
+    $('select#users').append($('<option></option>').val(username).html(username));
+    if (notify && (thisUser !== username) && (thisUser !== 'everyone')){
+        $('#user-list').append('<span class="admin">==>' + username + " just joined <==<br/>")
+    }
+}
+
+function sendMessage() {
+    var targetUser = $('#users').val();
+    $('form').submit(function(){
+        socket.emit('sendMessage', 
+         {
+                  "userSockets": true,
+                  "source": "",
+                  "message": $('input#msg').val(),
+                  "target": trgtUser
+                });
+	    $('#m').val("");
+    })
+}
+
+function addMessage(msg){
+    var html;
+    if (msg.target == "everyone") {
+        html = "<span class='to-all'>" + msg.source + " : " + msg.message + "</span><br/>"
+    } else {
+        // private message
+        html = "<span class='private'>" + msg.source + " (private) : " + msg.message + "</span><br/>"
+    }
+  $('#log').append(html);
+}
+
+*/
+
+socket.on('connect', newUser);
+//socket.on('updateLog', loadMessage)
+socket.on('updateUsers', updateUsers);
+
+
+// ENABLE USERNAME INPUT ONLY IF USER HAS BEEN SUBMITTED FOR THE CHAT!!!!!!!!!!!
